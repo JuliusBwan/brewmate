1a2977f061875cb02fcc81cf261c43d3859bea89
JuliusB <bwanikajulius2@gmail.com>
2026-07-31 17:59:56 +0100

    Chapter 2: price list - our first real program

--- price_list.py
+++ price_list.py
@@ -1,16 +1,29 @@
+
 #BrewMate Ltd -- Official Price List
 #Chapter 2: our first real program
 
-print("=" * 40)
+from datetime import date
+today = date.today()
+
+print("*" * 40)
+print("THE DATE TODAY: ", today)
+print("*" * 10)
+print("WELCOME TO THE BEST COFFEE")
+
+
+print("=" * 10)
 print("BREWMATE LTD -- PRICE LIST")
 print("=" * 40)
 print("\nCOFFEE MACHINES (one-time purchase)")
 print("Piccolo ........£450")
 print("Mezzo ..........£1200") #Sarah: note the 2
-print("Grande .........£2800")
+print("Grande .........£2800  SALE")
 print("\nBEAN SUBSCRIPTIONS (per month)")
 print("Starter ........£49 (2kg)")
 print("Professional ...£129 (6kg)")
 print("Enterprise .....£299 (15kg + Priority support)")
 print("\nAll prices exclude VAT (20%)") #Marcus insisted on this line
 
+print("*" * 40)
+print("For contact: +234850580")
+print("*" * 40)
